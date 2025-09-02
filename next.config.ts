53e0dcd4d55548eea317f975b3c8f906864aeec6
Audu samaila <154596278+litcode-dev@users.noreply.github.com>
2025-09-02 14:50:06 +0100

    language updated

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 const { i18n } = require('./next-i18next.config');
 
 const nextConfig: NextConfig = {
+  i18n,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://api.kasuabuy.com',
   },
